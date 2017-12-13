839afd26f530d6148fe569942d44e8b0cb4e09f0
Alexander Shepetko <a@shepetko.com>
2017-12-13 17:01:43 +0200

    Release 1.1

--- __init__.py
+++ __init__.py
@@ -1,15 +1,18 @@
 """PytSite Telegram Content Export Driver
 """
-from pytsite import lang as _lang
-from plugins import content_export as _content_export
-from . import _driver
 
 __author__ = 'Alexander Shepetko'
 __email__ = 'a@shepetko.com'
 __license__ = 'MIT'
 
-# Resources
-_lang.register_package(__name__)
 
-# Content export driver
-_content_export.register_driver(_driver.Driver())
+def plugin_load():
+    from pytsite import lang
+    from plugins import content_export
+    from . import _driver
+
+    # Resources
+    lang.register_package(__name__)
+
+    # Content export driver
+    content_export.register_driver(_driver.Driver())
